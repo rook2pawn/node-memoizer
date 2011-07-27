e3c93dad2943620128c613b4b382bfc15fcfccc4
david wee <rook2pawn@gmail.com>
2011-07-27 01:34:32 -0700

    added package.json

---
+++ README.md
@@ -0,0 +1,23 @@
+memoizer - f2 = memo(f); f2 is now fully memoized!
+==================================================
+From [Wikipedia] [wikipedia-memo]
+<cite>In computing, memoization is an optimization technique used primarily to speed up computer programs by having function calls avoid repeating the calculation of results for previously-processed inputs.</cite> 
+
+example:
+
+	var memo = require('memoizer');
+	var f = function(x,y,z) { return ((x + 1) * y) + z };
+	
+	var f2 = memo(f);
+
+	// now f2 is fully memoized!
+
+	f2(2,3,4);
+	
+	>> 13
+
+	f2(2,3,4);
+	
+	>> 13 // but instantly from cache!
+
+[wikipedia-memo]: http://en.wikipedia.org/wiki/Memoization
--- test/test.js
+++ test/test.js
@@ -1,11 +1,13 @@
 var memo = require('../index');
 var foo = function(x,y,z) { return (x*y) + z }
 var bar = function(fn,y) { return fn(y)};
-var foo_memoized = Memoizer(foo);
-var bar_memoized = Memoizer(bar);
-
-console.log(foo_memoized(2,3,4)); // 10, calculates it
-console.log(foo_memoized(2,3,4)); // 10 instantly!
-console.log(bar_memoized(function(val) { return val + 300 },7));
-console.log(bar_memoized(function(val) { return val + 300 },7));
-
+var foo_memoized = memo(foo);
+var bar_memoized = memo(bar);
+exports.testStoreAndFetch = function(test) {
+	test.expect(4);
+	test.equal(10,foo_memoized(2,3,4));
+	test.equal(10,foo_memoized(2,3,4));
+	test.equal(307,bar_memoized(function(val) { return val + 300 },7));
+	test.equal(307,bar_memoized(function(val) { return val + 300 },7));
+	test.done();
+};
